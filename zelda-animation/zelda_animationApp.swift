80351f3cc05b27a500750fd71541eed94c4aebf9
Diego Zea <dzea@pibox.app>
2024-03-06 21:39:46 -0500

    Initial Commit

---
+++ zelda-animation/zelda_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  zelda_animationApp.swift
+//  zelda-animation
+//
+//  Created by Diego Zea on 6/03/24.
+//
+
+import SwiftUI
+
+@main
+struct zelda_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
